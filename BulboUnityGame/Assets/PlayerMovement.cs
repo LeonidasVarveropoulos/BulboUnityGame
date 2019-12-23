02347b50e4e54d3d7c22bb6cbbe5efbaeccb2226
LeonidasVarveropoulos <leonidas@vrhome.net>
2019-12-23 14:21:13 -0600

    added new asset

---
+++ BulboUnityGame/Assets/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    float horizontalMove = 0f;
+
+    public float runSpeed = 40f;
+    public float gravity = 3.0f;
+    bool jump = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // Move the charcter
+
+        if (controller.isGrounded() && horizontalMove == 0f)
+        {
+            GetComponent<Rigidbody2D>().gravityScale = 0.0f;
+            GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
+        }
+        else
+            GetComponent<Rigidbody2D>().gravityScale = gravity;
+            GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
+
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+
+        jump = false;
+
+    }
+}
